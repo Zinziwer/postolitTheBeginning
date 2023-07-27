bda33c58cec5323775f20c920c42493878764c1c
Zinziwer <unislon@mail.ru>
2023-07-28 02:45:38 +0300

    Chapter4.1 - 4.6

--- hell/firstapp/views.py
+++ hell/firstapp/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World! This  is my first project on Django!")
+    return HttpResponse("<h2>Main</h2>")
+def about(request):
+    return HttpResponse("<h2>About</h2>")
+def contact(request):
+    return HttpResponse("<h2>Contacts</h2>")
+def products(request, productid=1):
+    output = "<h2>Product № {0}</h2>".format(productid)
+    return HttpResponse(output)
+def users(request, id=1, name='Maxim'):
+    output = "<h2>User</h2><h3>id № {0} Name: {1}</h3>".format(id, name)
+    return HttpResponse(output)
--- hell/hell/urls.py
+++ hell/hell/urls.py
@@ -15,9 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
 from firstapp import views
 
 urlpatterns = [
-    path('',views.index, name='home'),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='home'),
+    re_path(r'^about', views.about),
+    re_path(r'^contact', views.contact),
+    path('products/', views.products),
+    path('products/<int:productid>/', views.products),
+    path('users/', views.users),
+    path('users/<int:id>/<name>/', views.users),
+
+    # re_path(r'^products/(?P<productid>\d+)/', views.products),
+
+    # re_path(r'^users/(?P<id>\d+)/(?P<name>\D+)', views.users),
 ]
